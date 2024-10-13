3246450bf142b07337a1ef0357902ef28a1ba8ea
danarseptiyanto <septiyandanar@gmail.com>
2024-10-14 06:06:56 +0700

    ssdf

---
+++ app/Models/Article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Article extends Model
+{
+    use HasFactory;
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->topNavigation()
             ->login()
             ->colors([
                 'primary' => Color::Amber,
